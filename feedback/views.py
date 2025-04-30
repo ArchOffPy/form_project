c4e27090ff198a34e3f599ec6400a818ad33dea5
Arch <vipusk123@gmail.com>
2025-04-30 20:17:44 +0700

    add template DONE, add new rout and fucn DONE

--- feedback/views.py
+++ feedback/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 def index(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        surname = request.POST['surname']
+        return HttpResponseRedirect('/done')
     return render(request, 'feedback/feedback.html')
+
+
+def done(request):
+    return render(request, 'feedback/done.html')
+
+
