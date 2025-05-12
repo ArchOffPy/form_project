c8078902fbec768d6c6f1f5c557da0631bc6276e
Arch <vipusk123@gmail.com>
2025-05-12 16:59:18 +0700

    created django forms and deleted HTML forms

--- feedback/views.py
+++ feedback/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from .forms import FeedbackForm
+
 
 def index(request):
+
     if request.method == 'POST':
-        name = request.POST['name']
-        if len(name) == 0:
-            return render(request, 'feedback/feedback.html', context={'error_name': True})
-        return HttpResponseRedirect('/done')
-    return render(request, 'feedback/feedback.html', context={'error_name': False})
+         # создаем экземпляр класса FeedbackForm, помещаем в него запрос
+        form = FeedbackForm(request.POST)
+        # таким образом проверяем, пустая ли форма. И если не пустая то обрабатываем
+        if form.is_valid():
+            # просто выведем в консоль
+            print(form.cleaned_data)
+            # возвращаем успешный шаблон
+            return HttpResponseRedirect('/done')
+    # иначе снова создаем пустую форму и отображаем ее в шаблоне
+    form = FeedbackForm()
+    return render(request, 'feedback/feedback.html', context={'form': form})
 
 
 def done(request):
