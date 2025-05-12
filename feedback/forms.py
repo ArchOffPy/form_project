990184bdc592773aa8ecf43be275206df3528f5b
Arch <vipusk123@gmail.com>
2025-05-12 17:38:15 +0700

    testing validate fields

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 
 class FeedbackForm(forms.Form):
-    name = forms.CharField()
-    surname = forms.CharField()
-    feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 3, 'cols': 20}))
-
+    name = forms.CharField(label='Имя', max_length=10, min_length=2, error_messages={
+        'max_length': 'Слишком дофига символов',
+        'min_length': 'Слишком мало символов',
+        'required': 'Слушай, поле нужно заполнить',
+    })
+    # surname = forms.CharField()
+    # feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 3, 'cols': 20}))
+    rating = forms.IntegerField(label='Рейтиг', max_value=5, min_value=1, error_messages={
+        'max_value': 'Слишком много. Нужно не более 5',
+        'min_value': 'Слишком мало. Нужно не менее 1',
+        'required': 'Нет, пустым оставить поле нельзя'
+    })
--- feedback/views.py
+++ feedback/views.py
@@ -6,16 +6,13 @@ from .forms import FeedbackForm
 def index(request):
 
     if request.method == 'POST':
-         # создаем экземпляр класса FeedbackForm, помещаем в него запрос
         form = FeedbackForm(request.POST)
-        # таким образом проверяем, пустая ли форма. И если не пустая то обрабатываем
         if form.is_valid():
-            # просто выведем в консоль
             print(form.cleaned_data)
-            # возвращаем успешный шаблон
             return HttpResponseRedirect('/done')
-    # иначе снова создаем пустую форму и отображаем ее в шаблоне
-    form = FeedbackForm()
+    else:
+        form = FeedbackForm()
+
     return render(request, 'feedback/feedback.html', context={'form': form})
 
 
